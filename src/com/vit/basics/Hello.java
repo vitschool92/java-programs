5a8e0c04a6a743cf01214d80b3042e2767261a7b
vitschool92 <vitschool92@gmail.com>
2023-11-30 12:12:20 +0530

    Added logic for first program

--- src/com/vit/basics/Hello.java
+++ src/com/vit/basics/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.print("Hello World");
 		System.out.println("\n My first program");
+		System.out.println("This my github class");
 		
 	}
 }
